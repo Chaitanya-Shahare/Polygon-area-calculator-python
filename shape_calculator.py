7d96a1a5be40a6ea2ad5b4412e688fa8630371e9
CHAITANYA SHAHARE <chaitanyashahare@CHAITANYAs-MacBook-Air.local>
2022-09-02 09:13:09 +0530

    Completed shape_calculator.py with both rectangle class and square subclass

--- shape_calculator.py
+++ shape_calculator.py
@@ -3,6 +3,9 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def __repr__(self): 
+        return f"Rectangle(width={self.width}, height={self.height})"
+
     def set_width(self, width):
         self.width = width
 
@@ -10,13 +13,16 @@ class Rectangle:
         self.height = height
 
     def get_area(self):
-        return (self.width * self.height)
+        area = (self.width * self.height)
+        return area
 
     def get_perimeter(self):
-        return (2 * self.width + 2 * self.height)
+        perimeter = (2 * self.width + 2 * self.height)
+        return perimeter
 
     def get_diagonal(self):
-        return ((width ** 2 + height ** 2) ** .5)
+        diagonal = ((self.width ** 2 + self.height ** 2) ** .5)
+        return diagonal
 
     def get_picture(self):
         if self.width > 50 or self.height > 50:
@@ -25,12 +31,36 @@ class Rectangle:
         picture = firstline * self.height
         return picture
 
-    # def get_amount_inside(self, instance):
+    def get_amount_inside(self, instance):
+        amount = int((self.width / instance.width) * (self.height / instance.height))
+        return amount
 
 
 
 
-# class Square:
+class Square(Rectangle):
+    def __init__(self, side):
+        self.side = side
+        self.width = self.side
+        self.height = self.side
+        Rectangle.__init__(self, self.width, self.height)
 
-rectangle = Rectangle(10, 5)
-print(rectangle.get_picture())
+    def set_side(self, side):
+        self.side = side
+        Rectangle.set_width(self, side)
+        Rectangle.set_height(self, side)
+
+    def __repr__(self): 
+        self.side = self.width
+        return f"Square(side={self.side})"
+
+
+
+
+# rectangle = Rectangle(10, 5)
+# square = Square(5)
+# square.set_side(2)
+# square.set_side(4)
+# print(rectangle.get_picture())
+# print(rectangle.get_amount_inside(square))
+# print(square.get_picture())
